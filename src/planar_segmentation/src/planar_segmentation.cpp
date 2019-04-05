2ed0d96c76803349e9da7599be584022e9687b8b
Yunfei <yunfei.shi@connect.polyu.hk>
2019-04-05 00:19:16 -0400

    add drawing

--- src/planar_segmentation/src/planar_segmentation.cpp
+++ src/planar_segmentation/src/planar_segmentation.cpp
@@ -15,6 +15,66 @@
 #include <vector>
 #include <cmath> 
 
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+float P [12] = {-0.14273474, -0.98966773, -0.01358608,  0.0585618 ,-0.10300689,  0.02850545, -0.99427211,  0.51491767, 0.98438629, -0.14051771, -0.10601131,  0.24758666}; 
+
+
+void drawBoundingBox(pcl::PointCloud<pcl::PointXYZ>::Ptr& plane0, pcl::PointCloud<pcl::PointXYZ>::Ptr& plane1, pcl::PointCloud<pcl::PointXYZ>::Ptr& plane2)
+{ 
+    std::vector<float> x_arr;
+    std::vector<float> y_arr;
+    float u;
+    float v;
+    float w;
+   
+    for (int i = 0; i < plane0->points.size(); i++){
+        u = plane0->points[i].x*P[0]+plane0->points[i].y*P[1]+plane0->points[i].z*P[2]+P[3];
+        v = plane0->points[i].x*P[4]+plane0->points[i].y*P[5]+plane0->points[i].z*P[6]+P[7];
+        w = plane0->points[i].x*P[8]+plane0->points[i].y*P[9]+plane0->points[i].z*P[10]+P[11];
+        x_arr.push_back(u/w);
+        y_arr.push_back(v/w);
+        std::cout<<u/w<<" "<<v/w<<std::endl;
+    }
+    for (int i = 0; i < plane1->points.size(); i++){
+        u = plane1->points[i].x*P[0]+plane1->points[i].y*P[1]+plane1->points[i].z*P[2]+P[3];
+        v = plane1->points[i].x*P[4]+plane1->points[i].y*P[5]+plane1->points[i].z*P[6]+P[7];
+        w = plane1->points[i].x*P[8]+plane1->points[i].y*P[9]+plane1->points[i].z*P[10]+P[11];
+        x_arr.push_back(u/w);
+        y_arr.push_back(v/w);
+        std::cout<<u/w<<" "<<v/w<<std::endl;
+    }
+    for (int i = 0; i < plane2->points.size(); i++){
+        u = plane2->points[i].x*P[0]+plane2->points[i].y*P[1]+plane2->points[i].z*P[2]+P[3];
+        v = plane2->points[i].x*P[4]+plane2->points[i].y*P[5]+plane2->points[i].z*P[6]+P[7];
+        w = plane2->points[i].x*P[8]+plane2->points[i].y*P[9]+plane2->points[i].z*P[10]+P[11];
+        x_arr.push_back(u/w);
+        y_arr.push_back(v/w);
+        std::cout<<u/w<<" "<<v/w<<std::endl;
+    }
+
+
+    cv::Mat image;
+    image = cv::imread("../Data/000_Color.png", CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        std::cout <<  "Could not open or find the image" << std::endl ;
+        return;
+    }
+
+    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+    for(std::vector<float>::size_type i=0; i < x_arr.size(); i++){
+      // x_loc=x_arr[i];
+      // y_loc=y_arr[i];
+      cv::circle(image, cv::Point(x_arr[i],y_arr[i]), 5, CV_RGB(0,255,0),-1);
+      }
+    cv::imshow( "Display window", image );                   // Show our image inside it.
+
+    cv::waitKey(0);  
+}
+
 
 inline double distance(pcl::PointXYZ &p)
 {
@@ -116,9 +176,9 @@ int main (int argc, char** argv)
 
   std::cerr << "Bounding Box Point cloud data: " << bb_cloud->points.size () << " points" << std::endl;
 
-  pcl::visualization::CloudViewer viewer1 ("Simple Cloud Viewer");
-  viewer1.showCloud (bb_cloud);
-  while (!viewer1.wasStopped ()) {}
+  // pcl::visualization::CloudViewer viewer1 ("Simple Cloud Viewer");
+  // viewer1.showCloud (bb_cloud);
+  // while (!viewer1.wasStopped ()) {}
 
   pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
   pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
@@ -133,27 +193,29 @@ int main (int argc, char** argv)
 
   //extract table
   pcl::PointCloud<pcl::PointXYZ>::Ptr table_cloud = extract_plane( bb_cloud, seg, coefficients);
-  pcl::visualization::CloudViewer viewer_plane ("Simple Cloud Viewer");
-  viewer_plane.showCloud (table_cloud);
-  while (!viewer_plane.wasStopped ()) {}
+  // pcl::visualization::CloudViewer viewer_plane ("Simple Cloud Viewer");
+  // viewer_plane.showCloud (table_cloud);
+  // while (!viewer_plane.wasStopped ()) {}
 
   //extract plane 0
   pcl::PointCloud<pcl::PointXYZ>::Ptr plane0_cloud = extract_plane( bb_cloud, seg, coefficients);
-  pcl::visualization::CloudViewer viewer_plane0 ("Simple Cloud Viewer");
-  viewer_plane0.showCloud (plane0_cloud);
-  while (!viewer_plane0.wasStopped ()) {}
+  // pcl::visualization::CloudViewer viewer_plane0 ("Simple Cloud Viewer");
+  // viewer_plane0.showCloud (plane0_cloud);
+  // while (!viewer_plane0.wasStopped ()) {}
 
   //extract plane 1
   pcl::PointCloud<pcl::PointXYZ>::Ptr plane1_cloud = extract_plane( bb_cloud, seg, coefficients);
-  pcl::visualization::CloudViewer viewer_plane1 ("Simple Cloud Viewer");
-  viewer_plane1.showCloud (plane1_cloud);
-  while (!viewer_plane1.wasStopped ()) {}
+  // pcl::visualization::CloudViewer viewer_plane1 ("Simple Cloud Viewer");
+  // viewer_plane1.showCloud (plane1_cloud);
+  // while (!viewer_plane1.wasStopped ()) {}
 
   //extract plane 2
   pcl::PointCloud<pcl::PointXYZ>::Ptr plane2_cloud = extract_plane( bb_cloud, seg, coefficients);
-  pcl::visualization::CloudViewer viewer_plane2 ("Simple Cloud Viewer");
-  viewer_plane2.showCloud (plane2_cloud);
-  while (!viewer_plane2.wasStopped ()) {}
-  
+  // pcl::visualization::CloudViewer viewer_plane2 ("Simple Cloud Viewer");
+  // viewer_plane2.showCloud (plane2_cloud);
+  // while (!viewer_plane2.wasStopped ()) {}
+  drawBoundingBox(plane0_cloud,plane1_cloud,plane2_cloud);
+
+
   return (0);
 }
