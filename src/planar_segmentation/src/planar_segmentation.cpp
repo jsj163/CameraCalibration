41cf91b27e32c69e5ffaad105f9e12ea6dd6810d
zyyy <youngzh@umich.edu>
2019-04-04 23:41:59 -0400

    revise coding style

--- src/planar_segmentation/src/planar_segmentation.cpp
+++ src/planar_segmentation/src/planar_segmentation.cpp
@@ -21,6 +21,57 @@ inline double distance(pcl::PointXYZ &p)
    return std::sqrt( p.x* p.x + p.y* p.y + p.z* p.z);
 }
 
+pcl::PointCloud<pcl::PointXYZ>::Ptr extract_plane(pcl::PointCloud<pcl::PointXYZ>::Ptr &cloud, pcl::SACSegmentation<pcl::PointXYZ> &seg, pcl::ModelCoefficients::Ptr &coefficients)
+{
+    pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_p (new pcl::PointCloud<pcl::PointXYZ>), cloud_f (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
+    seg.setInputCloud (cloud);
+    seg.segment (*inliers, *coefficients);
+
+    if (inliers->indices.size () == 0)
+    {
+      PCL_ERROR ("Could not estimate a planar model for the given dataset.");
+      return plane;
+    }
+
+    std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
+                                        << coefficients->values[1] << " "
+                                        << coefficients->values[2] << " " 
+                                        << coefficients->values[3] << std::endl;
+
+    std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
+  
+    plane->points.resize (inliers->indices.size ());
+    for (size_t i = 0; i < inliers->indices.size (); ++i) plane->points[i] = cloud->points[inliers->indices[i]];
+    float x = 0.0;
+    float y = 0.0;
+    float z = 0.0;
+    for (int i = 0; i < plane->points.size(); i++){
+        x += plane->points[i].x;
+        y += plane->points[i].y;
+        z += plane->points[i].z;
+    }
+    x = x / plane->points.size();
+    y = y / plane->points.size();
+    z = z / plane->points.size();
+    std::cout<<"Centroid: "<<x<<' '<<y<<" "<<z<<std::endl;
+    
+    pcl::ExtractIndices<pcl::PointXYZ> extract;
+    // Extract the inliers
+    extract.setInputCloud (cloud);
+    extract.setIndices (inliers);
+    extract.setNegative (false);
+    extract.filter (*cloud_p);
+
+    // Create the filtering object
+    extract.setNegative (true);
+    extract.filter (*cloud_f);
+    cloud.swap (cloud_f);
+
+    return plane;
+}
+
 int main (int argc, char** argv)
 {
   
@@ -30,6 +81,8 @@ int main (int argc, char** argv)
   Reader.read("../Data/000.pcd", *cloud);
 
   std::cerr << "Point cloud data: " << cloud->points.size () << " points" << std::endl;
+
+  //2D bounding box segmentation
 
   double x_l = 180.0;
   double x_u = 380.0;
@@ -42,7 +95,6 @@ int main (int argc, char** argv)
   pcl::PointXYZ minPt, maxPt;
   pcl::getMinMax3D (*cloud, minPt, maxPt);
 
-
   double z_min = maxPt.z + 0.5/3.0*(minPt.z - maxPt.z);
 
   double x_min = minPt.x + x_l/x_range*(maxPt.x - minPt.x);
@@ -52,6 +104,7 @@ int main (int argc, char** argv)
   double y_max = minPt.y + y_u/y_range*(maxPt.y - minPt.y);
 
   pcl::PointCloud<pcl::PointXYZ>::Ptr bb_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
   // BoundingBox Point cloud
   for (int i = 0; i < cloud->points.size(); i++)
   { 
@@ -78,58 +131,29 @@ int main (int argc, char** argv)
   seg.setMethodType (pcl::SAC_RANSAC); 
   seg.setDistanceThreshold (0.01);
 
-  // Create the filtering object
-  pcl::ExtractIndices<pcl::PointXYZ> extract;
-  while(bb_cloud->points.size() >= 500)
-  {
-    seg.setInputCloud (bb_cloud);
-    seg.segment (*inliers, *coefficients);
+  //extract table
+  pcl::PointCloud<pcl::PointXYZ>::Ptr table_cloud = extract_plane( bb_cloud, seg, coefficients);
+  pcl::visualization::CloudViewer viewer_plane ("Simple Cloud Viewer");
+  viewer_plane.showCloud (table_cloud);
+  while (!viewer_plane.wasStopped ()) {}
 
-    if (inliers->indices.size () == 0)
-    {
-      PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-      return (-1);
-    }
+  //extract plane 0
+  pcl::PointCloud<pcl::PointXYZ>::Ptr plane0_cloud = extract_plane( bb_cloud, seg, coefficients);
+  pcl::visualization::CloudViewer viewer_plane0 ("Simple Cloud Viewer");
+  viewer_plane0.showCloud (plane0_cloud);
+  while (!viewer_plane0.wasStopped ()) {}
 
-    std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
-                                        << coefficients->values[1] << " "
-                                        << coefficients->values[2] << " " 
-                                        << coefficients->values[3] << std::endl;
+  //extract plane 1
+  pcl::PointCloud<pcl::PointXYZ>::Ptr plane1_cloud = extract_plane( bb_cloud, seg, coefficients);
+  pcl::visualization::CloudViewer viewer_plane1 ("Simple Cloud Viewer");
+  viewer_plane1.showCloud (plane1_cloud);
+  while (!viewer_plane1.wasStopped ()) {}
 
-    std::cerr << "Model inliers: " << inliers->indices.size () << std::endl;
-  
-    pcl::PointCloud<pcl::PointXYZ>::Ptr plane(new pcl::PointCloud<pcl::PointXYZ>);
-    plane->points.resize (inliers->indices.size ());
-    for (size_t i = 0; i < inliers->indices.size (); ++i) plane->points[i] = bb_cloud->points[inliers->indices[i]];
-  
-    pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-    viewer.showCloud (plane);
-    float x = 0.0;
-    float y = 0.0;
-    float z = 0.0;
-    for (int i = 0; i < plane->points.size(); i++){
-        x += plane->points[i].x;
-        y += plane->points[i].y;
-        z += plane->points[i].z;
-    }
-    x = x / plane->points.size();
-    y = y / plane->points.size();
-    z = z / plane->points.size();
-    std::cout<<"Centroid: "<<x<<' '<<y<<" "<<z<<std::endl;
-    while (!viewer.wasStopped ()) {}
-
-    // Extract the inliers
-    extract.setInputCloud (bb_cloud);
-    extract.setIndices (inliers);
-    extract.setNegative (false);
-    extract.filter (*cloud_p);
-
-    // Create the filtering object
-    extract.setNegative (true);
-    extract.filter (*cloud_f);
-    bb_cloud.swap (cloud_f);
-  }
-
+  //extract plane 2
+  pcl::PointCloud<pcl::PointXYZ>::Ptr plane2_cloud = extract_plane( bb_cloud, seg, coefficients);
+  pcl::visualization::CloudViewer viewer_plane2 ("Simple Cloud Viewer");
+  viewer_plane2.showCloud (plane2_cloud);
+  while (!viewer_plane2.wasStopped ()) {}
   
   return (0);
 }
